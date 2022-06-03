996c94cb7fad5a518e93653c35177a4bcc270a92
Deb the DBA <dgmelkin@gmail.com>
2022-06-03 11:06:29 -0400

    Adding code generated as part of the DBAVUG session to the branch

---
+++ Superheroes/Superheroes/Staging/Tables/Good_Deed_Load.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Staging].[Good_Deed_Load] (
+    [First_Name]            VARCHAR (50)  NULL,
+    [Last_Name]             VARCHAR (50)  NULL,
+    [Good_Deed_Type_Name]   VARCHAR (50)  NULL,
+    [Good_Deed_Description] VARCHAR (250) NULL,
+    [Good_Deed_Timestamp]   DATETIME      NULL
+);
+
